11aaab1bafeb5c8ec3ffc069170e54c9bc2bfe8e
Cristiam Camilo Samboni <ccsambo106@gmail.com>
2022-11-02 21:06:04 -0500

    Se agrega el proyecto.

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function cambiarModo() { 
+    var cuerpoweb = document.body; 
+    cuerpoweb.classList.toggle("oscuro"); 
+}
+
+function cambiarModo() { 
+        var cuerpoweb = document.body; 
+        cuerpoweb.classList.toggle("oscuro"); 
+} 
